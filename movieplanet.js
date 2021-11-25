69619659585afe7946c15dffd278054bfaefdfc1
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-25 11:42:42 -0800

    movieplanet revise#4

--- movieplanet.js
+++ movieplanet.js
@@ -144,3 +144,50 @@ export default class extends React.Component{
         )
     };
 }
+
+
+/* get detail data */
+export default class extends React.Component{
+    constructor(props) {
+        super(props);
+        const {location: { pathname }} = props;     // to use pahtname (= url), bring value from props which is given by react router defaultly
+        this.state = {
+            result: null,
+            error: null,
+            loading: true,
+            isMovie: pathname.includes("/movie/"),
+        };
+    }
+    async componentDidMount() {
+        const { 
+            match: { 
+                params: { id } 
+            }, 
+            history: { push },    
+        } = this.props;
+        const { isMovie } = this.state;
+        const parsedId = parseInt(id);
+        if(isNaN(parsedId)) {
+            return push("/");                   // id is equal to number, push to 'home' if id is typed other than number
+        }
+        let result = null;                                                  // make result variable which can be changed to movie or show result by using 'let'
+        try{
+            if(isMovie) {
+                ({data: result} = await moviesApi.movieDetail(parsedId));   // this is set resut got from API. then override 'null' value result to result with data
+            } else {
+                ({data: result} = await tvApi.showDetail(parsedId));
+            }
+        } catch{
+            this.setState({ error: "Can't find anything."})
+        } finally{
+            this.setState({ loading: false, result})
+        }
+    }
+    render() {
+        const { result, error, loading } = this.state;
+        console.log(result);
+        return (
+            <DetailPresenter  result={result}  error={error}  loading={loading} />
+        )
+    };
+}
