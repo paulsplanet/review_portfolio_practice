8e2e009fdf431bcfc1d9c774c7c4c5ded8b45a6b
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-27 18:03:50 -0800

    planetblog - revise#2

--- planetblog.js
+++ planetblog.js
@@ -1,14 +1,3 @@
-/* useSelector and useDispatch */
-const Header = () => {
-    const user  = useSelector(state => state.auth.user.id );            // useSelector: select state from Redux.
-    const dispatch = useDispatch();                                     // declare dispatch by using useDispatch
-    return(
-        <>
-            ...
-            <HeaderButton onClick={() => dispatch(cleanForm('user'))}>Logout</HeaderButton>         // use dispatch to redux Action
-        </>
-    )}
-
 
 /* Redux Store - under modules folder */
 import { createAction, handleActions } from "redux-actions";
@@ -62,3 +51,93 @@ const rootReducer = combineReducers({
     post,
 });
 export default rootReducer;
+
+
+/* changeField Action & initializedField Action */
+export const changeField = createAction(CHANGE_FILED, ({ key, value }) => ({ key, value }) );
+export const initializeField = createAction(INITIALIZE_FIELD);
+
+const post = handleActions({
+    [CHANGE_FILED]: (state, { payload: { key, value } }) => ({
+        ...state,
+        [key]: value,                                           // didn't use the produce because of initialState structure { title: '', body: '', tags: [], posts: [{title, body, tags, ...}, {}, {}]}                        
+    }),                                                         // first 3 states (tilte, body, tags) are for input elements to type in.
+    [POST_WRITING]: (state, { payload: post }) => ({
+        ...state,
+        posts: state.posts.concat(post),                        // using concat (not push)
+    }), 
+    [INITIALIZE_FIELD]: (state) => ({
+        ...state,
+        title: initialState.title,
+        body: initialState.body,
+        tags: initialState.tags,
+    })
+}, initialState)
+
+
+/* container structure */
+import React, { useEffect } from "react";
+import LoginForm from "../../Components/LoginForm";
+import { connect } from "react-redux";
+import { loginForm, changeInput, cleanForm } from "../../modules/auth";                         // import actions which exported in 'auth' store
+
+const LoginContainer = ({ user, login, loginForm, changeInput, cleanForm, history }) => {
+    useEffect(() => {
+        if(user.id) history.push('/')
+    }) 
+    return (
+        <>
+        <LoginForm   form={login} onLogin={loginForm} onChangeInput={changeInput} onClean={cleanForm} />    // give props to child
+        </>
+    )
+};
+
+export default connect(
+   ({ auth }) => ({                             // bring states in store
+       login: auth.login,
+       user: auth.user
+   }),
+   {                                            // bring actions in store
+       changeInput,
+       loginForm,
+       cleanForm,
+   }
+)(LoginContainer);
+
+
+/* select several store by using useSelector */
+const PostListContainer = () => {
+    const { user, posts } = useSelector( ({ auth, post }) => ({
+        user: auth.user,
+        posts: post.posts,
+    }))
+}
+
+
+/* match prop - from react router */
+const PostViewerContainer = (match) => {
+    const posts = useSelector( state => state.post.posts)           // select posts state in post store
+    const id = match.match.params.postId;
+    const selectPost = posts.filter(function(post) {     
+        return String(post._id) === id
+    })
+    return(
+        <>
+            <Header />
+            <PostViewer post={selectPost} />
+        </>
+    )
+}
+
+
+/* useSelector and useDispatch */
+const Header = () => {
+    const user  = useSelector(state => state.auth.user.id );            // useSelector: select state from Redux.
+    const dispatch = useDispatch();                                     // declare dispatch by using useDispatch
+    return(
+        <>
+            ...
+            <HeaderButton onClick={() => dispatch(cleanForm('user'))}>Logout</HeaderButton>         // use dispatch to redux Action
+        </>
+    )}
+
