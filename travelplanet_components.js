68b17fe5ac32afe6cf4fce4fdba687c43342ff6b
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-30 16:47:18 -0800

    paulsplanet componenet - revise#2

--- travelplanet_components.js
+++ travelplanet_components.js
@@ -30,3 +30,87 @@ function FileUpload (props) {
         props.refreshFunction(newImages);
     }
 }
+
+
+/*  CartPage    */
+// UserCardBlock
+function UserCardBlock (props) {
+    const renderCartImage = (images) => {
+        if(images.length > 0) {
+            let image = images[0];
+            return `http://localhost:5000/${image}`;
+    }}
+    const renderItems = () => (
+        props.products && props.products.map((product, index) => (                  // double checking = execute only when there is data
+            <tr key={index}>
+                <td>
+                    <img style={{ width: '150px' }} alt="product" src={renderCartImage(product.images)} />
+                </td>
+                <td>
+                    {product.title} 
+                </td>
+                <td>
+                    {product.quantity} EA
+                </td>
+                <td>
+                    $ {product.price}
+                </td>
+                <td>
+                    <button onClick={() => props.removeItem(product._id)}>
+                        Remove
+                    </button>
+                </td>
+            </tr>
+)))}
+//  CartPage
+function CartPage (props) {
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        let cartItems = [];
+        if(props.user.userData && props.user.userData.cart) {               // double checking data
+            if(props.user.userData.cart.length > 0) {
+                props.user.userData.cart.forEach(item => {
+                    cartItems.push(item.id)
+                })
+                dispatch(getCartItems(cartItems, props.user.userData.cart))     // dispatch actions 
+                    .then(response => { calculateTotal(response.payload) })
+            }
+        }
+    }, [props.user.userData])
+}
+
+
+/*  DetailProductPage   */
+function DetailProductPage (props) {
+    const productId = props.match.params.productId;
+    const  [product, setProduct] = useState({});
+    useEffect(() => {
+        axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
+            .then(response => {
+                setProduct(response.data[0])                            // get data by using axios.get, then save it to local state.
+            })
+            .catch(err => alert(err))
+    }, [])
+    return (
+        <div style={{ width: '100%', padding: '3rem 4rem'}}>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <h1>{product.title}</h1>
+            </div>
+            <br />
+            <Row gutter={[16, 16]}> 
+                <Col lg={12} sm={24}>
+                    {/* product image */}
+                    <ProductImage detail={product} />               // give data as prop which I got it by axios.
+                </Col>
+
+                <Col lg={12} sm={24}>
+                    {/* product info */}
+                    <ProductInfo detail={product} />
+                </Col>
+            </Row>
+        </div>
+    )
+};
+
+export default DetailProductPage;
